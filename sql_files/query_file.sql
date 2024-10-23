0968ebd3aa6ae940fa50d6f1b9a84164a3f6715e
nsogaard <82528923+NSogaard@users.noreply.github.com>
2024-10-22 20:07:07 -0400

    Final Touches To The Assignment
    I added the following to the assignment files as final touches before submission:
    1) I added the README file that is required in all assignments for this course
    2) I added the new version of my ERD image as well as the code that was used to generate it
    3) I added names to all of the columns in my query_file.sql so that the output of all of the queries is more readable.

--- sql_files/query_file.sql
+++ sql_files/query_file.sql
@@ -14,38 +14,38 @@ CREATE VIEW employee_personal_info AS
 	SELECT emp_no, first_name, last_name
 	FROM employees;
 
-SELECT dept_man.dept_no, depts.dept_name, dept_man.emp_no, epi.last_name, epi.first_name
+SELECT dept_man.dept_no AS "Department Number", depts.dept_name AS "Department Name", dept_man.emp_no AS "Employee Number", epi.last_name AS "Last Name", epi.first_name AS "First Name"
 FROM department_managers AS dept_man
 INNER JOIN departments AS depts ON depts.dept_no = dept_man.dept_no
 INNER JOIN employee_personal_info AS epi ON epi.emp_no = dept_man.emp_no;
 
 -- Query #4: List the department number for each employee along with that employee’s employee number, last name, first name, and department name
-SELECT dept_emp.dept_no, dept_emp.emp_no, epi.last_name, epi.first_name, depts.dept_name
+SELECT dept_emp.dept_no AS "Department Number", dept_emp.emp_no AS "Employee Number", epi.last_name AS "Last Name", epi.first_name AS "First Name", depts.dept_name AS "Department Name"
 FROM department_employees AS dept_emp
 INNER JOIN departments AS depts ON depts.dept_no = dept_emp.dept_no
 INNER JOIN employee_personal_info AS epi ON epi.emp_no = dept_emp.emp_no;
 
 -- Query #5: List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
-SElECT first_name, last_name, sex
+SElECT first_name AS "First Name", last_name AS "Last Name", sex AS "Sex"
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- Query #6: List each employee in the Sales department, including their employee number, last name, and first name
-SELECT dept_emp.emp_no, epi.last_name, epi.first_name, depts.dept_name
+SELECT dept_emp.emp_no AS "Employee Number", epi.last_name AS "Last Name", epi.first_name AS "First Name", depts.dept_name AS "Department Name"
 FROM department_employees AS dept_emp
 INNER JOIN departments AS depts ON depts.dept_no = dept_emp.dept_no
 INNER JOIN employee_personal_info AS epi ON epi.emp_no = dept_emp.emp_no
 WHERE depts.dept_name = 'Sales';
 
 -- Query #7: List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
-SELECT dept_emp.emp_no, epi.last_name, epi.first_name, depts.dept_name
+SELECT dept_emp.emp_no AS "Employee Number", epi.last_name AS "Last Name", epi.first_name AS "First Name", depts.dept_name AS "Department Number"
 FROM department_employees AS dept_emp
 INNER JOIN departments AS depts ON depts.dept_no = dept_emp.dept_no
 INNER JOIN employee_personal_info AS epi ON epi.emp_no = dept_emp.emp_no
 WHERE depts.dept_name = 'Sales' OR depts.dept_name = 'Development';
 
 -- Query #8: List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) 
-SELECT last_name, COUNT(*)
+SELECT last_name AS "Last Name", COUNT(*) AS "Last Name Count"
 FROM employees
 GROUP BY last_name
 ORDER BY COUNT(*) DESC;
